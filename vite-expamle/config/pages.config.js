c5931763645d112dc66662a1d5e243d406d0515c
ekas <mail@mail.com>
2023-10-21 12:01:33 +0300

    занятия 08-09

---
+++ vite-expamle/config/pages.config.js
@@ -0,0 +1,14 @@
+// import { resolve } from 'path'
+
+// const pages = [
+//     {name: 'main', path: resolve(__dirname, '../index.html')},
+//     {name: 'about', path: resolve(__dirname, '../pages/about.html')},
+//     {name: 'secondary', path: resolve(__dirname, '../pages/secondary.html')},
+//     {name: 'page-1', path: resolve(__dirname, '../pages/articles/page-1.html')},
+//     {name: 'page-2', path: resolve(__dirname, '../pages/articles/page-2.html')},
+//     {name: 'page-3', path: resolve(__dirname, '../pages/articles/page-3.html')},
+//     {name: 'page-4', path: resolve(__dirname, '../pages/articles/page-3.html')},
+//     {name: 'page-5', path: resolve(__dirname, '../pages/articles/page-4.html')},
+// ];
+
+// export default pages
