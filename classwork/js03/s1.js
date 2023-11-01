20ac360aea17ceb84862c1b34672dcb442f19a72
ekas <mail@mail.com>
2023-11-01 13:03:31 +0300

    3 занятие по js

---
+++ classwork/js03/s1.js
@@ -0,0 +1,67 @@
+
+// let perem
+// const k = 0
+
+// let perem1 = 'asdasd'
+// let perem2 = 0
+// let perem3 = true
+
+// perem = 12
+
+// perem = perem + 1 //13
+// perem += 1
+
+
+// perem = 'sad'
+
+
+// let isSad = (perem === 'sad')
+// isSad //true
+
+// isSad ? console.log('это правда') : console.log('это не правда')
+
+// console.log('это ' + (isSad ? '' : 'не') + ' правда')
+
+// if (isSad) {
+//     console.log('это правда')
+// } else {
+//     console.log('это не правда')
+// }
+
+// let num = 1
+// let i = 99
+
+// for (let i=0; i<10; i++) {
+//     console.log(i)
+
+//     num += 1 
+// }
+// console.log('результат', num)
+
+// console.log('i', i)
+
+// {
+//     console.log('i', i)
+// }
+
+// console.log('i', i)
+
+// alert('wqeqwe')
+
+function abc () {
+    let result = 0
+
+    result += 10
+    console.log('text')
+    console.log(result)
+
+    return
+}
+
+let a = abc()
+
+console.log(a)
+
+
+
+
